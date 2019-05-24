8de8092557a9aec63aea29008940b6f691fe6a67
kzuu <47418500+kzuu@users.noreply.github.com>
2019-05-24 19:49:15 +0300

    Delete ktoggle.sh

--- polybar/ktoggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $(xmodmap | grep control | awk -F ' ' '{print $2'}) == "Control_L" ]]
-	then
-		xmodmap -e "keycode 37 = Control_R" && xmodmap -e "keycode 105 = Control_L"
-	else
-		xmodmap -e "keycode 105 = Control_R" && xmodmap -e "keycode 37 = Control_L"
-fi
